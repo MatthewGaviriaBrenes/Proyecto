78167e2fe5f84be203260c512e1ffeca0249765f
MatthewGaviriaBrenes <105175799+MatthewGaviriaBrenes@users.noreply.github.com>
2022-11-24 09:37:37 -0600

    Add files via upload

---
+++ Proyecto/Models/InformesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Proyecto.Models
+{
+	public class InformesModel
+	{
+		//[Key]
+		//[Required]
+		public string numinform { get; set; } = "";
+
+		//public string ejtxt { get; set; }
+		
+		//public InformesModel()
+		//{
+		//}
+	}
+}
+
